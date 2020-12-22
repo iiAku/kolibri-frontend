3c86918e1c2784b758d316cf51b93c33e09fcc28
Ryan <ryan@calidog.io>
2020-12-21 22:08:00 -0500

    Flatten docs (#272)

--- vue-frontend/src/pages/ProjectInfo.vue
+++ vue-frontend/src/pages/ProjectInfo.vue
@@ -45,7 +45,7 @@ const allDocs = (ctx => {
   let keys = ctx.keys();
   let values = keys.map(ctx);
   return keys.reduce((o, k, i) => { o[k] = values[i]; return o; }, {});
-})(require.context('../../../documentation/online-docs', true, /.*/));
+})(require.context('../../../documentation', true, /.md$/));
 
 export default {
   name: 'ProjectInfo',
