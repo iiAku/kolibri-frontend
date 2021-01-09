9545c4f118e3db17721376a4bbdd3e4e4a99834e
Ryan Sears <ryan@calidog.io>
2021-01-09 04:04:45 -0500

    Good lord what a rabbithole
    Turns out browser support is sketchy for regex look-behinds, and docker's M1 support has a buggy filesystem 🙃

--- vue-frontend/src/mixins.js
+++ vue-frontend/src/mixins.js
@@ -19,7 +19,9 @@ export default {
             this.$swal(title, errString, 'error');
         },
         numberWithCommas(str) {
-            return str.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+            let parts = str.toString().split(".");
+            parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            return parts.join(".");
         },
         borrowedTokens(ovenAddress){
             if (!ovenAddress) { return 0 }
