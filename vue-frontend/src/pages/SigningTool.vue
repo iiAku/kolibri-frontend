c16c6400a249e7b0b2bc00c02045a6abf948843c
Ryan Sears <ryan@calidog.io>
2021-02-17 17:03:49 -0500

    Remove some testing stuff from signing utility

--- vue-frontend/src/pages/SigningTool.vue
+++ vue-frontend/src/pages/SigningTool.vue
@@ -20,7 +20,7 @@
                   <textarea v-text="decodedMessage" readonly class="textarea" placeholder="Your Decoded Payload Will Appear Here"></textarea>
                 </div>
               </div>
-              <div class="field">
+              <div v-if="false" class="field">
                 <label class="label">Human Readable</label>
                 <div class="control">
                   <template v-if="message !== null">
@@ -58,14 +58,16 @@
 
 <script>
   import { TezosLanguageUtil } from 'conseiljs'
-  import { addressDecoder } from "@taquito/local-forging/dist/lib/codec";
-  import { Uint8ArrayConsumer } from "@taquito/local-forging";
+  // import { addressDecoder } from "@taquito/local-forging/dist/lib/codec";
+  // import { Uint8ArrayConsumer } from "@taquito/local-forging";
+  // import { MichelsonMap } from "@taquito/michelson-encoder";
+  // import { Parser } from '@taquito/michel-codec'
 
   export default {
     name: "SigningTool",
     data() {
       return {
-        message: '0x0507070a00000004a836502107070002020000007f0320053d036d0743036e0a0000001601aec9fbb01f9f21d3c61b2379c13ddf40de32fc25000655036e0000001425736574476f7665726e6f72436f6e74726163740200000010072f0200000004034f032702000000000743036a00000743036e0a00000016010c21fa1ba5104ea31fcd8b4741f7e84fec3ce9d300034d031b',
+        message: null,
         signatureResult: null,
       }
     },
@@ -75,46 +77,57 @@
       }
     },
     computed: {
-      humanReadableMessage(){
-        try {
-
-          console.log(
-              addressDecoder(
-                  new Uint8ArrayConsumer.fromHexString('01e5fd66cc8fd280028be25a706ecd9f5a6c3e563700')
-              )
-          )
-          const parsedMsg = JSON.parse(
-              TezosLanguageUtil.normalizeMichelineWhiteSpace(
-                  TezosLanguageUtil.hexToMicheline(this.message.substr(4)).code
-              )
-          )
-          console.log(parsedMsg)
-
-          const lambdaContents = parsedMsg.args[1].args[1]
-
-          if (lambdaContents.length !== 9){
-            throw Error("Unknown contract type! This is an early tool :grimace:")
-          }
-
-          return {
-            chainID: "0x" + parsedMsg.args[0].bytes,
-            operationID: parseInt(parsedMsg.args[1].args[0].int),
-            functionAddress: addressDecoder(
-                new Uint8ArrayConsumer.fromHexString(parsedMsg.args[1].args[1][2].args[1].bytes)
-            ),
-            functionCall: parsedMsg.args[1].args[1][3].annots[0],
-            mutezToSend: parseInt(parsedMsg.args[1].args[1][5].args[1].int),
-            toAddress: addressDecoder(
-                new Uint8ArrayConsumer.fromHexString(parsedMsg.args[1].args[1][6].args[1].bytes)
-            ),
-            // lambdaContents,
-            // parsedMsg
-          }
-        } catch(e) {
-          console.log(e)
-          return {}
-        }
-      },
+      // humanReadableMessage(){
+      //   try {
+      //
+      //     console.log(
+      //         addressDecoder(
+      //             new Uint8ArrayConsumer.fromHexString('01e5fd66cc8fd280028be25a706ecd9f5a6c3e563700')
+      //         )
+      //     )
+      //     const parsedMsg = JSON.parse(
+      //         TezosLanguageUtil.normalizeMichelineWhiteSpace(
+      //             TezosLanguageUtil.hexToMicheline(this.message.substr(4)).code
+      //         )
+      //     )
+      //     // const rawMsg = TezosLanguageUtil.normalizeMichelineWhiteSpace(
+      //     //     TezosLanguageUtil.hexToMicheline(this.message.substr(4)).code
+      //     // )
+      //     console.log(parsedMsg)
+      //
+      //     debugger;
+      //
+      //     const p = new Parser()
+      //     let res = p.parseJSON(parsedMsg)
+      //     console.log(res)
+      //     const parsed = new MichelsonMap(parsedMsg)
+      //     console.log(parsed)
+      //
+      //     const lambdaContents = parsedMsg.args[1].args[1]
+      //
+      //     if (lambdaContents.length !== 9){
+      //       throw Error("Unknown contract type! This is an early tool :grimace:")
+      //     }
+      //
+      //     return {
+      //       chainID: "0x" + parsedMsg.args[0].bytes,
+      //       operationID: parseInt(parsedMsg.args[1].args[0].int),
+      //       functionAddress: addressDecoder(
+      //           new Uint8ArrayConsumer.fromHexString(parsedMsg.args[1].args[1][2].args[1].bytes)
+      //       ),
+      //       functionCall: parsedMsg.args[1].args[1][3].annots[0],
+      //       mutezToSend: parseInt(parsedMsg.args[1].args[1][5].args[1].int),
+      //       toAddress: addressDecoder(
+      //           new Uint8ArrayConsumer.fromHexString(parsedMsg.args[1].args[1][6].args[1].bytes)
+      //       ),
+      //       // lambdaContents,
+      //       // parsedMsg
+      //     }
+      //   } catch(e) {
+      //     console.log(e)
+      //     return {}
+      //   }
+      // },
       decodedMessage(){
         if (this.message === null || !this.message.startsWith('0x')){
           return null
