3f7d3b003b9a0d6592133f0f87bfa68c59473ca2
Ryan Sears <ryan@calidog.io>
2021-09-20 22:45:48 -0400

    Match wrangler site parity with the gov portal

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,4 +1,4 @@
-import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
 
 /**
  * The DEBUG flag will do two things that help during development:
@@ -24,15 +24,18 @@ addEventListener('fetch', event => {
   }
 })
 
-async function handleEvent(event) {
-  const url = new URL(event.request.url)
-  let options = {}
+function addSecurityHeaders(response){
+  response.headers.set('X-XSS-Protection', '1; mode=block')
+  response.headers.set('X-Content-Type-Options', 'nosniff')
+  response.headers.set('X-Frame-Options', 'SAMEORIGIN')
+  response.headers.set('Referrer-Policy', 'same-origin')
+  response.headers.set('Feature-Policy', 'none')
+  response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains')
+  return response
+}
 
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
+async function handleEvent(event) {
+  let options = {}
 
   try {
     if (DEBUG) {
@@ -47,14 +50,7 @@ async function handleEvent(event) {
     // allow headers to be altered
     const response = new Response(page.body, page)
 
-    response.headers.set('X-XSS-Protection', '1; mode=block')
-    response.headers.set('X-Content-Type-Options', 'nosniff')
-    response.headers.set('X-Frame-Options', 'SAMEORIGIN')
-    response.headers.set('Referrer-Policy', 'same-origin')
-    response.headers.set('Feature-Policy', 'none')
-    response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains')
-
-    return response
+    return addSecurityHeaders(response)
 
   } catch (e) {
     // if an error is thrown try to serve the asset at 404.html
@@ -64,31 +60,13 @@ async function handleEvent(event) {
           mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/index.html`, req),
         })
 
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
-      } catch (e) {}
+        let response = new Response(notFoundResponse.body, { ...notFoundResponse, status: 200 })
+        return addSecurityHeaders(response)
+      } catch (e) {
+        console.error(e)
+      }
     }
 
     return new Response(e.message || e.toString(), { status: 500 })
-  }
-}
-
-/**
- * Here's one example of how to modify a request to
- * remove a specific prefix, in this case `/docs` from
- * the url. This can be useful if you are deploying to a
- * route on a zone, or if you only want your static content
- * to exist at a specific path.
- */
-function handlePrefix(prefix) {
-  return request => {
-    // compute the default (e.g. / -> index.html)
-    let defaultAssetKey = mapRequestToAsset(request)
-    let url = new URL(defaultAssetKey.url)
-
-    // strip the prefix from the path for lookup
-    url.pathname = url.pathname.replace(prefix, '/')
-
-    // inherit all other props from the default request
-    return new Request(url.toString(), defaultAssetKey)
   }
 }
