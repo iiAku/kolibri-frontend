2d0e3436c0bdf4b059a8e7c064d544bf1846a19f
Keefer Taylor <keefer@keefertaylor.com>
2022-02-16 15:19:22 -0500

    revert FORCE_MAINNET

--- src/store.js
+++ src/store.js
@@ -15,8 +15,8 @@ import { TezosToolkit } from "@taquito/taquito";
 import BigNumber from "bignumber.js";
 import _ from 'lodash'
 
-const FORCE_MAINNET = true
-// const FORCE_MAINNET = false
+// const FORCE_MAINNET = true
+const FORCE_MAINNET = false
 
 function dontIndexTestnets() {
     // If we're in testnet tell google not to index
